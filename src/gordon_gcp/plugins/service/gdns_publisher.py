7e1752a3924d8a4692cd7c6dc158ccbf7f94f2ad
Danielle Zephyr Malament <danielle.malament@gmail.com>
2018-06-07 21:27:49 -0400

    Change some variable handling in the GDNS publisher

--- src/gordon_gcp/plugins/service/gdns_publisher.py
+++ src/gordon_gcp/plugins/service/gdns_publisher.py
@@ -41,14 +41,8 @@ from gordon_gcp.clients import auth
 from gordon_gcp.clients import http
 from gordon_gcp.plugins import _utils
 
-__all__ = ('GDNSPublisher', 'GDNSPublisherBuilder')
 
-BASE_CHANGES_ENDPOINT = ('https://www.googleapis.com/dns/{version}/projects/'
-                         '{project}/managedZones/{managedZone}/changes')
-RESOURCE_RECORDS_ENDPOINT = ('https://www.googleapis.com/dns/v1/projects/'
-                             '{project}/managedZones/{managedZone}/rrsets')
-# see https://cloud.google.com/dns/api/v1/changes#resource
-DNS_CHANGES_DONE = 'done'
+__all__ = ('GDNSPublisher', 'GDNSPublisherBuilder')
 
 
 class GDNSPublisherBuilder:
@@ -172,6 +166,13 @@ class GDNSPublisher:
             A Google HTTP connection class.
     """
     phase = 'publish'
+    BASE_CHANGES_ENDPOINT = ('https://www.googleapis.com/dns/{version}'
+                             '/projects/{project}/managedZones/{managedZone}'
+                             '/changes')
+    RESOURCE_RECORDS_ENDPOINT = ('https://www.googleapis.com/dns/v1/projects/'
+                                 '{project}/managedZones/{managedZone}/rrsets')
+    # see https://cloud.google.com/dns/api/v1/changes#resource
+    DNS_CHANGES_DONE = 'done'
 
     def __init__(self, config, success_channel, error_channel, http_client):
         self.config = config
@@ -179,16 +180,9 @@ class GDNSPublisher:
         self.error_channel = error_channel
         self.http_client = http_client
         self.publish_wait_timeout = self.config.get('publish_wait_timeout', 60)
-        self.managed_zone = self.config['managed_zone']
-        self.dns_zone = self.config['dns_zone']
         self.project = self.config['project']
         self.default_ttl = self.config['default_ttl']
         self.api_version = self.config.get('api_version', 'v1')
-        self.base_changes_url = BASE_CHANGES_ENDPOINT.format(
-            version=self.api_version, project=self.project,
-            managedZone=self.managed_zone)
-        self.resource_records_url = RESOURCE_RECORDS_ENDPOINT.format(
-            project=self.project, managedZone=self.managed_zone)
         self._logger = logging.getLogger('')
 
     # TODO: This will be eventually moved to GEventMessage
@@ -216,11 +210,12 @@ class GDNSPublisher:
             action: [resource_record]
         }
 
-    async def _publish_changes(self, changes):
+    async def _publish_changes(self, changes, base_changes_url):
         """Publish changes to the Google DNS Changes API.
 
         Args:
             changes (dict): The DNS record changes to publish.
+            base_changes_url (str): The URL to use to publish changes.
 
         Returns:
             change_id (str): Change ID of the Changes resource.
@@ -231,7 +226,7 @@ class GDNSPublisher:
         """
         try:
             resp = await self.http_client.request(
-                'post', self.base_changes_url, json=changes)
+                'post', base_changes_url, json=changes)
         except exceptions.GCPHTTPError as e:
             error_msg = e.args[0]
             # Extract status code from error message.
@@ -249,7 +244,7 @@ class GDNSPublisher:
         # TODO: create another task to measure propagation time
         return resp_dict['id']
 
-    async def _handle_additions_conflict(self, changes):
+    async def _handle_additions_conflict(self, changes, resource_records_url):
         """Compensate for an additions Changes request that returns an HTTP
            409.
 
@@ -258,11 +253,13 @@ class GDNSPublisher:
 
         Args:
             changes (dict): Changes containing an addition conflict.
+            resource_records_url (str): The URL to use to get the zone's
+                resource records.
 
         Returns:
             dict containing original additions plus deletions.
         """
-        resp = await self.http_client.get_all(self.resource_records_url)
+        resp = await self.http_client.get_all(resource_records_url)
         rrsets = resp['rrsets']
 
         record_name = changes['additions'][0]['name']
@@ -275,55 +272,67 @@ class GDNSPublisher:
 
         return changes
 
-    async def _changes_published(self, change_id):
+    async def _changes_published(self, change_id, base_changes_url):
         """Checks if a given Change resource has a completed status.
 
         See DNS_CHANGES_DONE.
 
         Args:
             change_id (str): Change ID of the Changes resource.
+            base_changes_url (str): The URL prefix to use to check on changes.
 
         Returns:
             True if the changes are done, False if anything else.
         """
-        change_id_url = f'{self.base_changes_url}/{change_id}'
+        change_id_url = f'{base_changes_url}/{change_id}'
         timeout = datetime.timedelta(seconds=self.publish_wait_timeout)
         start = datetime.datetime.now()
         end = start + timeout
 
         while datetime.datetime.now() < end:
             resp = await self.http_client.get_json(change_id_url)
-            if resp['status'] == DNS_CHANGES_DONE:
+            if resp['status'] == GDNSPublisher.DNS_CHANGES_DONE:
                 return True
             await asyncio.sleep(1)
 
         return False
 
-    async def _dispatch_changes(self, resource_record, action, logger):
+    async def _dispatch_changes(self, resource_record, managed_zone, action,
+                                logger):
         """Publish changes for one record, making sure they are completed.
 
         Args:
             resource_record (dict): A resource record to change.
-            action: The action to take (e.g. 'additions').
-            logger: An object to log with.
+            managed_zone (str): The managed zone name in Google DNS.
+            action (str): The action to take (e.g. 'additions').
+            logger (object): An object to log with.
 
         Raises:
             GCPPublishRecordTimeoutError if publishing of records exceeds wait
                 timeout.
         """
+        base_changes_url = GDNSPublisher.BASE_CHANGES_ENDPOINT.format(
+            version=self.api_version, project=self.project,
+            managedZone=managed_zone)
+        resource_records_url = GDNSPublisher.RESOURCE_RECORDS_ENDPOINT.format(
+            project=self.project, managedZone=managed_zone)
+
         changes_to_publish = self._format_resource_record_changes(
             action, resource_record)
         try:
-            change_id = await self._publish_changes(changes_to_publish)
+            change_id = await self._publish_changes(changes_to_publish,
+                                                    base_changes_url)
         except exceptions.GCPHTTPConflictError as e:
             msg = ('Conflict found when publishing records. Handling and '
                    'retrying.')
             logger.info(msg)
             changes_to_publish = await self._handle_additions_conflict(
-                changes_to_publish)
-            change_id = await self._publish_changes(changes_to_publish)
+                changes_to_publish, resource_records_url)
+            change_id = await self._publish_changes(changes_to_publish,
+                                                    base_changes_url)
 
-        change_complete = await self._changes_published(change_id)
+        change_complete = await self._changes_published(change_id,
+                                                        base_changes_url)
         if not change_complete:
             msg = ('Timed out while waiting for DNS changes to transition '
                    'to \'done\' status.')
@@ -348,10 +357,11 @@ class GDNSPublisher:
 
         for resource_record in event_msg.data['resourceRecords']:
             record_name = resource_record['name']
-            if not record_name.endswith('.' + self.dns_zone):
+            if not record_name.endswith('.' + self.config['dns_zone']):
                 msg = f'Error when asserting zone for record: {record_name}.'
                 msg_logger.error(msg)
                 raise exceptions.InvalidDNSZoneInMessageError(msg)
 
             await self._dispatch_changes(
-                resource_record, event_msg.data['action'], msg_logger)
+                resource_record, self.config['managed_zone'],
+                event_msg.data['action'], msg_logger)
