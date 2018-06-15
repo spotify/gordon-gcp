26a48307fc6e398b8c62742aa939cdcde802b06f
Amy Skinner <skinner@spotify.com>
2018-06-15 18:08:30 -0400

    Reorder args for GCEEnricher

--- tests/unit/plugins/service/test_enricher.py
+++ tests/unit/plugins/service/test_enricher.py
@@ -71,10 +71,14 @@ def mock_async_sleep(mocker, create_mock_coro):
     return sleep_mock
 
 
+@pytest.fixture
+def gce_enricher(config, metrics, mock_http_client):
+    return enricher.GCEEnricher(config, metrics, mock_http_client)
+
+
 @pytest.mark.asyncio
-async def test_handle_message_doesnt_need_enriching(mocker, caplog, config,
-                                                    gevent_msg):
-    gce_enricher = enricher.GCEEnricher(config, mocker.Mock(), None)
+async def test_handle_message_doesnt_need_enriching(mocker, caplog, gevent_msg,
+                                                    gce_enricher):
     gevent_msg.data['resourceRecords'] = [mocker.Mock()]
     expected_msg = 'Message already enriched, skipping phase.'
     await gce_enricher.handle_message(gevent_msg)
@@ -87,15 +91,15 @@ async def test_handle_message_doesnt_need_enriching(mocker, caplog, config,
     (2, 5)])
 @pytest.mark.asyncio
 async def test_handle_message_event_msg(
-        mocker, config, gevent_msg, mock_async_sleep, mock_http_client, caplog,
-        instance_data, sleep_calls, logs_logged, metrics):
+        mocker, config, gevent_msg, mock_async_sleep, gce_enricher, caplog,
+        instance_data, sleep_calls, logs_logged):
     """Successfully enrich event message."""
     instance_mocked_data = []
     for i in range(sleep_calls):
         instance_mocked_data.append({})
     instance_mocked_data.append(instance_data)
-    mock_http_client._get_json_mock.side_effect = instance_mocked_data
-    gce_enricher = enricher.GCEEnricher(config, mock_http_client, metrics)
+
+    gce_enricher._http_client._get_json_mock.side_effect = instance_mocked_data
 
     await gce_enricher.handle_message(gevent_msg)
 
@@ -124,11 +128,10 @@ async def test_handle_message_event_msg(
     ([{}] * 5, 4, 5, 'KeyError: \'networkInterfaces\'')])
 @pytest.mark.asyncio
 async def test_handle_message_event_msg_failures(
-        mocker, config, gevent_msg, mock_async_sleep, mock_http_client,
-        caplog, response, sleep_calls, logs_logged, err_msg, metrics):
+        mocker, gevent_msg, mock_async_sleep, gce_enricher, caplog, response,
+        sleep_calls, logs_logged, err_msg):
     """Raise error while enriching event message."""
-    mock_http_client._get_json_mock.side_effect = response
-    gce_enricher = enricher.GCEEnricher(config, mock_http_client, metrics)
+    gce_enricher._http_client._get_json_mock.side_effect = response
     gevent_msg.phase = 'enrich'
 
     with pytest.raises(exceptions.GCPGordonError) as e:
