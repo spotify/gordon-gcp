5600ada83504f7bb118f67eed48601f560c88bbf
Danielle Zephyr Malament <danielle.malament@gmail.com>
2018-09-13 12:29:07 -0400

    Prepare 0.0.1.dev24 release

--- src/gordon_gcp/__init__.py
+++ src/gordon_gcp/__init__.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 __author__ = 'Lynn Root'
-__version__ = '0.0.1.dev23'
+__version__ = '0.0.1.dev24'
 __license__ = 'Apache 2.0'
 __email__ = 'lynn@spotify.com'
 __description__ = 'GCP Plugins for Gordon and Gordon Janitor'
